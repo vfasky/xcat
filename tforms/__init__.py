4eda36336154512c866cd23dfd5e735797a42e93
vfasky <vfasky@gmail.com>
2013-05-30 17:35:52 +0800

    form use tforms

---
+++ tforms/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Version : $Id$
+# @See     : https://github.com/lepture/tforms
+# A form framework for tornado, based on tforms.
+__version__ = 0.1
